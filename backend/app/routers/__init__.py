8cf6dc9e2b01591c730716d15b41a0c1ca0b9ae5
guojunjiang-collab <guojun.jiang@qq.com>
2026-06-26 19:42:02 +0800

    chore: 删除旧路由 /parts /assemblies

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,7 +1,5 @@
 from .auth import router as auth_router
 from .users import router as users_router
-from .parts import router as parts_router
-from .assemblies import router as assemblies_router
 from .bom import router as bom_router
 from .logs import router as logs_router
 from .custom_fields import router as custom_fields_router
@@ -17,4 +15,4 @@ from .configuration import router as config_router
 from .inventory import router as inventory_router
 from .components import router as components_router
 
-__all__ = ["auth_router", "users_router", "parts_router", "assemblies_router", "bom_router", "logs_router", "custom_fields_router", "documents_router", "user_groups_router", "dashboard_router", "ecr_router", "eco_router", "config_router", "inventory_router", "components_router"]
+__all__ = ["auth_router", "users_router", "bom_router", "logs_router", "custom_fields_router", "documents_router", "user_groups_router", "dashboard_router", "ecr_router", "eco_router", "config_router", "inventory_router", "components_router"]
